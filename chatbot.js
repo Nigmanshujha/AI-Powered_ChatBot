9a1323dcf576b1f00a17a7661182ac31185ea8e8
Nigmanshujha <146453732+Nigmanshujha@users.noreply.github.com>
2025-04-22 14:04:49 +0530

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -8,7 +8,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const apiKey = process.env.GOOGLE_API_KEY;
+const API_KEY = "AIzaSyBqUUGzKXBGhdUoiHIqskk_5qier0XGrig";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
